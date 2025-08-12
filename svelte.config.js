5f5156aed9edd4c141a8d4699d665836e452f29a
Josh Nicholas <hi@thambi.li>
2025-08-12 20:17:04 +1000

    update

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : ''
+      base: process.env.NODE_ENV === 'production' ? '/flat-bread' : ''
     }
   }
 };
